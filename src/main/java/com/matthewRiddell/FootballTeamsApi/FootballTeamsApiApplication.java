ffab3eb7d8768561e4bf816ef8838b95e5d86c09
unknown <matthewriddell1@outlook.com>
2020-09-02 19:36:56 +0100

    initial commit including project generated via spring init

---
+++ src/main/java/com/matthewRiddell/FootballTeamsApi/FootballTeamsApiApplication.java
@@ -0,0 +1,13 @@
+package com.matthewRiddell.FootballTeamsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTeamsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballTeamsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matthewRiddell/FootballTeamsApi/FootballTeamsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthewRiddell.FootballTeamsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTeamsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
